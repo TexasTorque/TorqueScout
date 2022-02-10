7bc4cbba52fe7767f7f49dc7d3bd8d6b2e2f913e
Suhas44 <suhas8@outlook.com>
2022-02-10 16:01:40 -0600

    Compiles to JAR

--- src/main/java/org/texastorque/App.java
+++ src/main/java/org/texastorque/App.java
@@ -69,7 +69,7 @@ public class App extends Application {
         switchToMain();
     }
 
-    public static void main(String[] args) {
+    public static void startApp(String[] args) {
         launch(args);
     }
 
